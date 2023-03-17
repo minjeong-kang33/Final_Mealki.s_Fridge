f242d2f644656f4d40b735efc5ca4d4a62ecbcd5
ITWILL <ITWILL@DESKTOP-FKD5EKF>
2023-03-17 09:52:52 +0900

    기본java파일생성

---
+++ Mealki.s_Fridge/src/main/java/com/itwillbs/domain/CustomerDTO.java
@@ -0,0 +1,132 @@
+package com.itwillbs.domain;
+
+public class CustomerDTO {
+	private String cust_num;
+	private String cust_name;
+	private String cust_gubun1;
+	private String cust_gubun2;
+	private String business_num;
+	private String boss_name;
+	private String cust_tel;
+	private String cust_uptae;
+	private String cust_jongmok;
+	private String man_name;
+	private String man_tel;
+	private String man_email;
+	private String fax;
+	private int cust_post_num;
+	private String cust_adress;
+	private String cust_adress2;
+	private String url_path;
+	private String remarks;
+	public String getCust_num() {
+		return cust_num;
+	}
+	public void setCust_num(String cust_num) {
+		this.cust_num = cust_num;
+	}
+	public String getCust_name() {
+		return cust_name;
+	}
+	public void setCust_name(String cust_name) {
+		this.cust_name = cust_name;
+	}
+	public String getCust_gubun1() {
+		return cust_gubun1;
+	}
+	public void setCust_gubun1(String cust_gubun1) {
+		this.cust_gubun1 = cust_gubun1;
+	}
+	public String getCust_gubun2() {
+		return cust_gubun2;
+	}
+	public void setCust_gubun2(String cust_gubun2) {
+		this.cust_gubun2 = cust_gubun2;
+	}
+	public String getBusiness_num() {
+		return business_num;
+	}
+	public void setBusiness_num(String business_num) {
+		this.business_num = business_num;
+	}
+	public String getBoss_name() {
+		return boss_name;
+	}
+	public void setBoss_name(String boss_name) {
+		this.boss_name = boss_name;
+	}
+	public String getCust_tel() {
+		return cust_tel;
+	}
+	public void setCust_tel(String cust_tel) {
+		this.cust_tel = cust_tel;
+	}
+	public String getCust_uptae() {
+		return cust_uptae;
+	}
+	public void setCust_uptae(String cust_uptae) {
+		this.cust_uptae = cust_uptae;
+	}
+	public String getCust_jongmok() {
+		return cust_jongmok;
+	}
+	public void setCust_jongmok(String cust_jongmok) {
+		this.cust_jongmok = cust_jongmok;
+	}
+	public String getMan_name() {
+		return man_name;
+	}
+	public void setMan_name(String man_name) {
+		this.man_name = man_name;
+	}
+	public String getMan_tel() {
+		return man_tel;
+	}
+	public void setMan_tel(String man_tel) {
+		this.man_tel = man_tel;
+	}
+	public String getMan_email() {
+		return man_email;
+	}
+	public void setMan_email(String man_email) {
+		this.man_email = man_email;
+	}
+	public String getFax() {
+		return fax;
+	}
+	public void setFax(String fax) {
+		this.fax = fax;
+	}
+	public int getCust_post_num() {
+		return cust_post_num;
+	}
+	public void setCust_post_num(int cust_post_num) {
+		this.cust_post_num = cust_post_num;
+	}
+	public String getCust_adress() {
+		return cust_adress;
+	}
+	public void setCust_adress(String cust_adress) {
+		this.cust_adress = cust_adress;
+	}
+	public String getCust_adress2() {
+		return cust_adress2;
+	}
+	public void setCust_adress2(String cust_adress2) {
+		this.cust_adress2 = cust_adress2;
+	}
+	public String getUrl_path() {
+		return url_path;
+	}
+	public void setUrl_path(String url_path) {
+		this.url_path = url_path;
+	}
+	public String getRemarks() {
+		return remarks;
+	}
+	public void setRemarks(String remarks) {
+		this.remarks = remarks;
+	}
+	
+
+}
