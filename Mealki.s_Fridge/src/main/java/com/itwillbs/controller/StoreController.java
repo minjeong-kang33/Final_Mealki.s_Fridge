3bf4a31040a3f51fb4ef7968b09639aa95ce42da
seungmin <sm.lee2026210@gmail.com>
2023-03-27 18:24:43 +0900

    [2023.03.27] store 표 수정으로 인한 컨트롤러 ~ 매퍼 수정

--- Mealki.s_Fridge/src/main/java/com/itwillbs/controller/StoreController.java
+++ Mealki.s_Fridge/src/main/java/com/itwillbs/controller/StoreController.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.itwillbs.domain.StoreDTO;
 import com.itwillbs.service.StoreService;
@@ -44,44 +45,25 @@ public class StoreController {
 		return "wms/store/findProduct";
 	}
 	
+	@ResponseBody
 	@RequestMapping(value = "/wms/store/addStore", method = RequestMethod.POST)
-	public String addStore(HttpServletRequest request, Model model,StoreDTO storeDTO) {
+	public String addStore(HttpServletRequest request, Model model) {
 		System.out.println("storeController addStore()");
 		
-		String[] tdArr = request.getParameterValues("tdArr");
-		for(int i=0; i<tdArr.length;i++) {
-			System.out.println(tdArr[i]);
-		}
+		 
+		 StoreDTO storeDTO = new StoreDTO();
+		 storeDTO.setOrder_num(request.getParameter("order_num"));
+		 //var item_name = tdArr[1]; //품명
+		 storeDTO.setSto_qty(Integer.parseInt(request.getParameter("order_qty")));
+		 //var stk_qnt = tdArr[3]; //창고재고수량
+		 storeDTO.setSto_progress(request.getParameter("sto_progress"));
+		 storeDTO.setSto_empNum(Integer.parseInt(request.getParameter("sto_empNum")));
+		 storeDTO.setSto_shelf(request.getParameter("sto_shelf"));
+		 storeDTO.setSto_shelfDetail(Integer.parseInt(request.getParameter("sto_shelfDetail")));
+		 
+//		storeService.insertStore(storeDTO);
 		
-		storeDTO.setOrder_num(tdArr[0]); //발주 관리번호 
-		storeDTO.setOrder_qty(Integer.parseInt(tdArr[2])); //발주 개수 
-		storeDTO.setSto_qty(Integer.parseInt(tdArr[3])); //입고수량
-		storeDTO.setSto_remaining(Integer.parseInt(tdArr[4])-storeDTO.getSto_qty());//잔여발주량 
-		
-		if(storeDTO.getSto_remaining()+storeDTO.getSto_qty()==storeDTO.getOrder_qty()) {
-			storeDTO.setSto_progress("입고완료");
-		} else {
-			storeDTO.setSto_progress("입고진행");
-		}
-		//발주개수보다 잔여발주량이 작으면 입고진행중
-		//발주 개수랑 잔여수량이 같으면 입고완료
-		
-		storeDTO.setSto_empNum(Integer.parseInt(tdArr[7]));//처리 작업자 사번
-		storeDTO.setSto_shelf(tdArr[8]);//선반위치
-		storeDTO.setSto_shelfDetail(Integer.parseInt(tdArr[9]));//선반 층
-		
-		/*
-		 * System.out.println("발주관리번호"+storeDTO.getOrder_num());
-		 * System.out.println("발주 개수"+storeDTO.getOrder_qty());
-		 * System.out.println("입고수량"+storeDTO.getSto_qty());
-		 * System.out.println("잔여발주량"+storeDTO.getSto_remaining());
-		 * System.out.println("진행상황"+storeDTO.getSto_progress());
-		 * System.out.println("작업자사번호"+storeDTO.getSto_empNum());
-		 */
-		
-		storeService.insertStore(storeDTO);
-		
-		return "redirect:/wms/store/insertStore";
+		return "1";
 	}
 
 }
