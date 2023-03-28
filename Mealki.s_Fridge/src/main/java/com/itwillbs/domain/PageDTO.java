f64dfd6088a9ad23c46285ca6c095e8b392300b3
hojeong <ghwjd20000@gmail.com>
2023-03-28 20:41:57 +0900

    PageDTO 수정

--- Mealki.s_Fridge/src/main/java/com/itwillbs/domain/PageDTO.java
+++ Mealki.s_Fridge/src/main/java/com/itwillbs/domain/PageDTO.java
@@ -28,6 +28,9 @@ public class PageDTO {
 	private String wo_num;
 	private int business_num;
 	private Date out_date;
+	private Date dorder_date;
+	private Date dout_date;
+
 	
 	private String Search;
 	
@@ -259,6 +262,26 @@ public class PageDTO {
 
 	public void setWhs_num(String whs_num) {
 		this.whs_num = whs_num;
+	}
+
+
+	public Date getDorder_date() {
+		return dorder_date;
+	}
+
+
+	public void setDorder_date(Date dorder_date) {
+		this.dorder_date = dorder_date;
+	}
+
+
+	public Date getDout_date() {
+		return dout_date;
+	}
+
+
+	public void setDout_date(Date dout_date) {
+		this.dout_date = dout_date;
 	}
 	
 }
