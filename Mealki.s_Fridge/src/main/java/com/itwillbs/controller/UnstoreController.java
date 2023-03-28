71780378a2f3037039303fbf2c25849ab80ce00d
seungmin2026210 <sm.lee2026210@gmail.com>
2023-03-28 19:40:27 +0900

    [2023.03.28] unstore 리스트 가져오기 완료

--- Mealki.s_Fridge/src/main/java/com/itwillbs/controller/UnstoreController.java
+++ Mealki.s_Fridge/src/main/java/com/itwillbs/controller/UnstoreController.java
@@ -1,5 +1,9 @@
 package com.itwillbs.controller;
 
+import java.util.List;
+import java.util.Map;
+
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
@@ -7,13 +11,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.itwillbs.service.UnstoreService;
+
 @Controller
 public class UnstoreController {
 	
+	
+	@Inject
+	private UnstoreService unstoreService;
+	
 	@RequestMapping(value = "/wms/unstore/insertUnstore", method = RequestMethod.GET)
-	public String insertUnStore(HttpServletRequest request, Model model) {
+	public String insertUnstore(HttpServletRequest request, Model model) {
 		System.out.println("UnstoreController insertUnstore");
 		
+		//미출고 리스트
+		List<Map<String, Object>> unstoreListUnreleased = unstoreService.getUnstoreListUnreleased();
+		model.addAttribute("unstoreListUnreleased", unstoreListUnreleased);
 		
 		return "wms/unstore/insertUnstore";
 	}
