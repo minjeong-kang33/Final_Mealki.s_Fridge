69f047367fe79b93bb87088954a5ac766d12004b
seungmin2026210 <sm.lee2026210@gmail.com>
2023-03-28 22:22:31 +0900

    [2023.03.28] recipe r_num으로 필요한 재료를 list에 담아오는 메서드 생성

--- Mealki.s_Fridge/src/main/java/com/itwillbs/dao/RecipeDAOImpl.java
+++ Mealki.s_Fridge/src/main/java/com/itwillbs/dao/RecipeDAOImpl.java
@@ -1,5 +1,7 @@
 package com.itwillbs.dao;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
@@ -10,6 +12,14 @@ public class RecipeDAOImpl implements RecipeDAO {
 
 	@Inject
 	private SqlSession sqlSession;
+	
 	private static final String namespace = "com.itwillbs.mappers.recipeMapper";
+
+	@Override
+	public List<String> getRecipLists(String r_num) {
+		System.out.println("RecipeDAOImpl getRecipLists");
+		return sqlSession.selectList(namespace+".getRecipLists");
+	}
+
 	
 }
--- Mealki.s_Fridge/src/main/java/com/itwillbs/service/RecipeService.java
+++ Mealki.s_Fridge/src/main/java/com/itwillbs/service/RecipeService.java
@@ -1,5 +1,9 @@
 package com.itwillbs.service;
 
+import java.util.List;
+
 public interface RecipeService {
 
+	List<String> getRecipLists(String r_num);
+	
 }
