276d8c1636654706e03a134050eeb74594de034b
minjeonggg33 <minjeonggg33@gmail.com>
2023-03-28 15:34:27 +0900

    [2023.03.28] 재고관리 페이지 기본파일 생성

---
+++ Mealki.s_Fridge/src/main/java/com/itwillbs/service/StockServiceImpl.java
@@ -0,0 +1,59 @@
+package com.itwillbs.service;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import com.itwillbs.dao.StockDAO;
+import com.itwillbs.domain.ItemDTO;
+import com.itwillbs.domain.PageDTO;
+import com.itwillbs.domain.StockDTO;
+import com.itwillbs.domain.WarehouseDTO;
+
+@Service
+public class StockServiceImpl implements StockService {
+	@Inject
+	private StockDAO stockDAO;
+
+	@Override
+	public List<WarehouseDTO> getWarehouseList() {
+		System.out.println("StockServiceImpl getWarehouseList");
+
+		return stockDAO.getWarehouseList();
+	}
+
+	@Override
+	public List<ItemDTO> getItem_typeList() {
+		System.out.println("StockServiceImpl getItem_typeList");
+		
+		return stockDAO.getItem_typeList();
+	}
+
+	@Override
+	public List<StockDTO> getStockList(PageDTO pageDTO) {
+		System.out.println("StockServiceImpl getStockList");
+		
+		int startRow=(pageDTO.getCurrentPage()-1)*pageDTO.getPageSize()+1;
+		int endRow = startRow+pageDTO.getPageSize()-1;
+		pageDTO.setStartRow(startRow);
+		pageDTO.setEndRow(endRow);
+		
+		return stockDAO.getStockList(pageDTO);
+	}
+
+	@Override
+	public int getStockListCount(PageDTO pageDTO) {
+		System.out.println("StockServiceImpl getStockListCount");
+		
+		return stockDAO.getStockListCount(pageDTO);
+	}
+
+	
+	
+	
+	
+
+}
