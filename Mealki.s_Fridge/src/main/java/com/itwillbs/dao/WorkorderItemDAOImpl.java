8e1e9a9f2fdc45a5d8a6e63ecf2393c725fce510
seungmin <sm.lee2026210@gmail.com>
2023-03-29 16:19:55 +0900

    [2023.03.29] instoreController 및 workorderitem 수정 메서드를 통해 작업지시완료에 필요한 품목의 개수를 가져오는작업(미완)

--- Mealki.s_Fridge/src/main/java/com/itwillbs/dao/WorkorderItemDAOImpl.java
+++ Mealki.s_Fridge/src/main/java/com/itwillbs/dao/WorkorderItemDAOImpl.java
@@ -7,6 +7,8 @@ import javax.inject.Inject;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
+import com.itwillbs.domain.WorkorderItemDTO;
+
 @Repository
 public class WorkorderItemDAOImpl implements WorkorderItemDAO{
 
@@ -14,6 +16,15 @@ public class WorkorderItemDAOImpl implements WorkorderItemDAO{
 	private SqlSession sqlSession;
 	
 	private static final String namespace="com.itwillbs.mappers.workorderItemMapper";
+
+	@Override
+	public List<WorkorderItemDTO> getItemList(String wo_num) {
+		System.out.println("WorkorderItemDAOImpl getItemList");
+		
+		System.out.println(wo_num);
+		
+		return sqlSession.selectList(namespace+".getItemList",wo_num);
+	}
 
 
 }
